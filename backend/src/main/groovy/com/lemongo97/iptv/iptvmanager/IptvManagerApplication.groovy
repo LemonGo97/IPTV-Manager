fc0a650996ca6703663818614a5dea691752d70c
lemongo97 <lemongo97@outlook.com>
2026-05-11 23:49:09 +0800

    first commit

---
+++ backend/src/main/groovy/com/lemongo97/iptv/iptvmanager/IptvManagerApplication.groovy
@@ -0,0 +1,13 @@
+package com.lemongo97.iptv.iptvmanager
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class IptvManagerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(IptvManagerApplication, args)
+    }
+
+}
---
+++ backend/src/test/groovy/com/lemongo97/iptv/iptvmanager/IptvManagerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.lemongo97.iptv.iptvmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IptvManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
