c02d854277a1e6b603a873fad5708617c439f3e6
lemongo97 <lemongo97@outlook.com>
2026-05-11 23:51:20 +0800

    first commit

---
+++ backend/src/main/java/com/lemongo97/iptv/iptvmanager/IptvManagerApplication.java
@@ -0,0 +1,14 @@
+package com.lemongo97.iptv.iptvmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IptvManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IptvManagerApplication
+                .class, args);
+    }
+
+}
