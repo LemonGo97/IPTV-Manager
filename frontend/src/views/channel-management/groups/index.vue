d16debaa8dd10d0e66068dfd190d8c6268953f73
lemongo97 <lemongo97@outlook.com>
2026-05-13 06:13:42 +0800

    新增频道相关的菜单及其空页面

---
+++ frontend/src/views/channel-management/groups/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <CommonPage>
+    <div class="p-16">
+      <n-alert type="info">
+        频道组管理页面 - 开发中
+      </n-alert>
+    </div>
+  </CommonPage>
+</template>
+
+<script setup>
+import { NAlert } from 'naive-ui'
+import { CommonPage } from '@/components'
+
+defineOptions({
+  name: 'ChannelGroups',
+})
+</script>
---
+++ frontend/src/views/channel-management/rules/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <CommonPage>
+    <div class="p-16">
+      <n-alert type="info">
+        频道处理规则页面 - 开发中
+      </n-alert>
+    </div>
+  </CommonPage>
+</template>
+
+<script setup>
+import { NAlert } from 'naive-ui'
+import { CommonPage } from '@/components'
+
+defineOptions({
+  name: 'ChannelRules',
+})
+</script>
