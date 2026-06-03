d566d1f41919ef72a1afa26a04a4afb3c243b512
lemongo97 <lemongo97@outlook.com>
2026-06-03 22:16:35 +0800

    增加新的延迟检测引擎

---
+++ backend/src/main/java/com/lemongo97/iptv/iptvmanager/okhttp/exception/OkHttpRequestException.java
@@ -0,0 +1,8 @@
+package com.lemongo97.iptv.iptvmanager.okhttp.exception;
+
+public class OkHttpRequestException extends RuntimeException {
+
+    public OkHttpRequestException(String message) {
+        super(message);
+    }
+}
