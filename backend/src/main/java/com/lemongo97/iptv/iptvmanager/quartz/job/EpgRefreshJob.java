e5223acfcee57352858bc638776246c3b4bbe634
lemongo97 <lemongo97@outlook.com>
2026-06-06 20:32:45 +0800

    epg 获取改为okhttp以简化代码

--- backend/src/main/java/com/lemongo97/iptv/iptvmanager/quartz/job/EpgRefreshJob.java
+++ backend/src/main/java/com/lemongo97/iptv/iptvmanager/quartz/job/EpgRefreshJob.java
@@ -6,20 +6,26 @@ import com.lemongo97.iptv.iptvmanager.entity.EpgProvider;
 import com.lemongo97.iptv.iptvmanager.mapper.EpgChannelMapper;
 import com.lemongo97.iptv.iptvmanager.mapper.EpgProgramMapper;
 import com.lemongo97.iptv.iptvmanager.mapper.EpgProviderMapper;
+import com.lemongo97.iptv.iptvmanager.okhttp.exception.OkHttpRequestException;
 import com.lemongo97.iptv.iptvmanager.parser.epg.EPGParser;
+import com.lemongo97.iptv.iptvmanager.parser.epg.entity.EPGChannel;
+import com.lemongo97.iptv.iptvmanager.parser.epg.entity.EPGProgramme;
 import com.lemongo97.iptv.iptvmanager.parser.epg.handler.EPGChannelHandler;
 import com.lemongo97.iptv.iptvmanager.parser.epg.handler.EPGProgrammeHandler;
 import com.lemongo97.iptv.iptvmanager.service.TaskProgressService;
+import com.lemongo97.iptv.iptvmanager.utils.OkHttpUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
@@ -38,7 +44,6 @@ public class EpgRefreshJob implements Job {
     private final EpgChannelMapper epgChannelMapper;
     private final EpgProgramMapper epgProgramMapper;
     private final TaskProgressService taskProgressService;
-    private final RestTemplate restTemplate;
 
     @Override
     public void execute(JobExecutionContext context) {
@@ -97,67 +102,69 @@ public class EpgRefreshJob implements Job {
             taskProgressService.updateProgress(taskId, 0, 10.0, "正在获取 EPG 数据...");
         }
 
+        EPGChannelHandler epgChannelHandler = new SimpleEPGChannelHandler(epgProvider, epgChannelMapper);
+        EPGProgrammeHandler epgProgrammeHandler = new SimpleEPGProgrammeHandler(epgProvider, epgProgramMapper);
+
         String url = epgProvider.getUrl();
         log.info("Fetching EPG data from: {}", url);
-        restTemplate.execute(
-                url,
-                HttpMethod.GET,
-                null,
-                (response) -> {
-                    InputStream inputStream;
-                    if (epgProvider.getType() == EpgProvider.Type.XMLTV_GZIP) {
-                        inputStream = new GzipCompressorInputStream(response.getBody());
-                    } else if (epgProvider.getType() == EpgProvider.Type.XMLTV) {
-                        inputStream = response.getBody();
-                    } else {
-                        return null;
-                    }
+        OkHttpClient client = OkHttpUtil.getClient();
+        try (Response response = client.newCall(new Request.Builder().get().url(url).build()).execute()) {
+            if (!response.isSuccessful())
+                throw new OkHttpRequestException("request failed with code " + response.code());
 
-                    EPGParser.parse(inputStream, StandardCharsets.UTF_8,
-                            (EPGChannelHandler) channel -> {
-                                EpgChannel epgChannel = new EpgChannel()
-                                        .setChannelId(channel.getId())
-                                        .setDeleted(false)
-                                        .setEpgSourceId(epgProvider.getId())
-                                        .setIcon(channel.getIcon() == null ? null : new EpgChannel.Icon()
-                                                .setSrc(channel.getIcon().getSrc())
-                                                .setWidth(channel.getIcon().getWidth())
-                                                .setHeight(channel.getIcon().getHeight()))
-                                        .setDisplayName(channel.getDisplayName() == null ? null : channel.getDisplayName().stream().map(n ->
-                                                new EpgChannel.DisplayName()
-                                                        .setLang(n.getLang())
-                                                        .setValue(n.getValue())).toList());
-                                epgChannelMapper.insert(epgChannel);
-                            },
-                            (EPGProgrammeHandler) programme -> {
-                                EpgProgram epgProgram = new EpgProgram()
-                                        .setEpgSourceId(epgProvider.getId())
-                                        .setChannelId(programme.getChannel())
-                                        .setStartTime(programme.getStart())
-                                        .setStopTime(programme.getStop())
-                                        .setTitle(programme.getTitle() == null ? null : new EpgProgram.Title()
-                                                .setLang(programme.getTitle().getLang())
-                                                .setValue(programme.getTitle().getValue()))
-                                        .setDescription(programme.getDesc() == null ? null : programme.getDesc().stream().map(n -> new EpgProgram.Desc()
-                                                .setLang(n.getLang())
-                                                .setValue(n.getValue())).toList());
-                            epgProgramMapper.insert(epgProgram);
+            InputStream responseInputStream = response.body().byteStream();
+            InputStream inputStream;
+            if (epgProvider.getType() == EpgProvider.Type.XMLTV_GZIP) {
+                inputStream = new GzipCompressorInputStream(responseInputStream);
+            } else if (epgProvider.getType() == EpgProvider.Type.XMLTV) {
+                inputStream = responseInputStream;
+            } else {
+                throw new RuntimeException("unknown EPG provider type: " + epgProvider.getType());
+            }
+            epgChannelMapper.deleteBySourceId(epgProvider.getId());
+            epgProgramMapper.deleteBySourceId(epgProvider.getId());
+            EPGParser.parse(inputStream, StandardCharsets.UTF_8, epgChannelHandler, epgProgrammeHandler, null);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-                    }, null);
-                    return null;
-                }
-        );
+    private record SimpleEPGChannelHandler(EpgProvider epgProvider,
+                                           EpgChannelMapper epgChannelMapper) implements EPGChannelHandler {
+        @Override
+        public void handle(EPGChannel channel) {
+            EpgChannel epgChannel = new EpgChannel()
+                    .setChannelId(channel.getId())
+                    .setDeleted(false)
+                    .setEpgSourceId(epgProvider.getId())
+                    .setIcon(channel.getIcon() == null ? null : new EpgChannel.Icon()
+                                                                .setSrc(channel.getIcon().getSrc())
+                                                                .setWidth(channel.getIcon().getWidth())
+                                                                .setHeight(channel.getIcon().getHeight()))
+                    .setDisplayName(channel.getDisplayName() == null ? null : channel.getDisplayName().stream().map(n ->
+                            new EpgChannel.DisplayName()
+                            .setLang(n.getLang())
+                            .setValue(n.getValue())).toList());
+            epgChannelMapper.insert(epgChannel);
+        }
+    }
 
-//        var request = new HttpGet(url);
-//        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-//
-//            var response = httpClient.execute(request);
-//            var entity = response.getEntity();
-//            if (entity == null) {
-//                throw new Exception("No content returned from EPG source");
-//            }
-//
-//            return org.apache.hc.core5.http.io.entity.EntityUtils.toString(entity, "UTF-8");
-//        }
+    private record SimpleEPGProgrammeHandler(EpgProvider epgProvider,
+                                             EpgProgramMapper epgProgramMapper) implements EPGProgrammeHandler {
+        @Override
+        public void handle(EPGProgramme programme) {
+            EpgProgram epgProgram = new EpgProgram()
+                    .setEpgSourceId(epgProvider.getId())
+                    .setChannelId(programme.getChannel())
+                    .setStartTime(programme.getStart())
+                    .setStopTime(programme.getStop())
+                    .setTitle(programme.getTitle() == null ? null : new EpgProgram.Title()
+                                                                    .setLang(programme.getTitle().getLang())
+                                                                    .setValue(programme.getTitle().getValue()))
+                    .setDescription(programme.getDesc() == null ? null : programme.getDesc().stream().map(n -> new EpgProgram.Desc()
+                                                                                                               .setLang(n.getLang())
+                                                                                                               .setValue(n.getValue())).toList());
+            epgProgramMapper.insert(epgProgram);
+        }
     }
 }
